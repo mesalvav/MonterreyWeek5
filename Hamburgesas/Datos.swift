d4c3195418f801d2f4beb5a30e68b30961b6ae48
Mario E Salvatierra V <MarioSalvatierra@marios-mbp-2>
2015-12-13 14:50:30 -0500

    commit 11

---
+++ Hamburgesas/Datos.swift
@@ -0,0 +1,17 @@
+//
+//  Datos.swift
+//  Hamburgesas
+//
+//  Created by Mario E Salvatierra V on 12/13/15.
+//  Copyright © 2015 Mario E Salvatierra V. All rights reserved.
+// edit 11
+//
+
+import Foundation
+ 
+class ColeccionDePaises {
+    
+    let paises = ["Nicaragua", "México","Venezuela"]
+    
+    
+}
